ca0362e675d40572c54cbee8b080de908515156e
italo <italo.ribeiro@rakuten.com>
2025-01-23 14:43:25 +0900

    initial commit

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM openjdk:17-jdk-slim
-VOLUME /tmp
-ARG JAR_FILE=target/*.jar
-COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java","-jar","/app.jar"]
+
+WORKDIR /app
+
+COPY target/api-0.0.1-SNAPSHOT.jar app.jar
+
+EXPOSE 8080
+
+ENTRYPOINT ["java", "-jar", "app.jar"]
---
+++ build-and-run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Building the Spring Boot application..."
+./mvnw clean package
+
+echo "Building the Docker image..."
+docker-compose build
+
+echo "Running the Docker container..."
+docker-compose up -d
+
+echo "Application is now running at http://localhost:8082"
